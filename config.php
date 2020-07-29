1278c061e39128aa866312c6961691c8f6d2e084
siubie <putraprima@gmail.com>
2020-07-29 18:30:01 +0700

    First Code dari tutorial

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+
+define('CLIENT_ID', 'BfhrVI8VQ_eqlz9InZv9ZA');
+define('CLIENT_SECRET', 'J0XLgw07W45i4HwMBteebHNH8jgEXFW1');
+define('REDIRECT_URI', 'https://polizoom.000webhostapp.com/callback.php');
