23c0107c3e36b7c045415eebff6a21cef3725c6c
siubie <putraprima@gmail.com>
2020-07-29 18:36:19 +0700

    ganti credential

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 require_once 'vendor/autoload.php';
 require_once "class-db.php";
 
-define('CLIENT_ID', 'BfhrVI8VQ_eqlz9InZv9ZA');
-define('CLIENT_SECRET', 'J0XLgw07W45i4HwMBteebHNH8jgEXFW1');
-define('REDIRECT_URI', 'https://polizoom.000webhostapp.com/callback.php');
+define('CLIENT_ID', 'isien');
+define('CLIENT_SECRET', 'isien');
+define('REDIRECT_URI', 'url callback');
